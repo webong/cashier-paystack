eb8ede215f308def752a2b3436888d1e7a22188d
wisdomanthoni <wizitendo10@gmail.com>
2019-02-22 02:11:53 +0100

    test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,9 +1,27 @@
 <?php
 namespace Wisdomanthoni\Cashier\Tests;
 
-use PHPUnit\Framework\TestCase as BaseTestCase;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-
+     /**
+     * Load package service provider
+     * @param  \Illuminate\Foundation\Application $app
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+    }
+    /**
+     * Load package alias
+     * @param  \Illuminate\Foundation\Application $app
+     * @return array
+     */
+    protected function getPackageAliases($app)
+    {
+        return [
+            'Paystack' => Paystack::class,
+        ];
+    }
 }
