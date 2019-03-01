8a1a20ce848e01680c5ad9667fd0e9d1d5d0fe4a
wisdomanthoni <wizitendo10@gmail.com>
2019-03-01 17:42:45 +0100

    add WebhookControllerTest

---
+++ tests/WebhookControllerTest.php
@@ -0,0 +1,38 @@
+<?php
+namespace Wisdomanthoni\Cashier\Tests;
+
+use Illuminate\Http\Request;
+use Wisdomanthoni\Cashier\Http\Controllers\WebhookController;
+
+class WebhookControllerTest extends TestCase
+{
+    public function testProperMethodsAreCalledBasedOnPaystackEvent()
+    {
+        $_SERVER['__received'] = false;
+        $request = Request::create(
+            '/', 'POST', [], [], [], [], json_encode(['event' => 'subscription.create', 'data' => []])
+        );
+        (new WebhookControllerTestStub)->handleWebhook($request);
+        $this->assertTrue($_SERVER['__received']);
+    }
+    public function testNormalResponseIsReturnedIfMethodIsMissing()
+    {
+        $request = Request::create(
+            '/', 'POST', [], [], [], [], json_encode(['event' => 'foo.bar', 'data' => []])
+        );
+        $response = (new WebhookControllerTestStub)->handleWebhook($request);
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+}
+
+class WebhookControllerTestStub extends WebhookController
+{
+    public function __construct()
+    {
+        // Prevent setting middleware...
+    }
+    public function handleSubscriptionCreate()
+    {
+        $_SERVER['__received'] = true;
+    }
+}
